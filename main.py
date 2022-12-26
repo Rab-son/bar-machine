7b44648578f1f8d021499962c9731cd146e1bdca
Rab-son <rabsonsayendajnr@gmail.com>
2022-12-26 23:53:23 +0200

    bet completed

--- main.py
+++ main.py
@@ -33,6 +33,18 @@ def get_number_of_lines():
 
 
 def get_bet():
+    while True:
+        amount = input("What would you like to bet? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <=  amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+        
+    return amount
 
 def main():
     balance = deposit()
