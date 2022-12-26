ff0ece81f8ab4efea7b2a5f8c96110158f21fbd7
Rab-son <rabsonsayendajnr@gmail.com>
2022-12-26 23:54:32 +0200

    more descriptive

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def get_number_of_lines():
 
 def get_bet():
     while True:
-        amount = input("What would you like to bet? $")
+        amount = input("What would you like to bet on each line? $")
         if amount.isdigit():
             amount = int(amount)
             if MIN_BET <=  amount <= MAX_BET:
@@ -50,7 +50,7 @@ def main():
     balance = deposit()
     lines = get_number_of_lines()
     bet = get_bet()
-    
+
     print(balance, lines)
 
 
