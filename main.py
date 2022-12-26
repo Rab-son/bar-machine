39bad3d2c783054f491dea5e7e2b5a140a7afd0a
Rab-son <rabsonsayendajnr@gmail.com>
2022-12-26 23:41:33 +0200

    testing completed

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ def deposit():
                 break
             else:
                 print("Amount must be greater than 0.")
-            
         else:
             print("Please enter a number.")
         
     return amount
+
+deposit()
