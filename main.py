02714d3283564bce1839cae7232888db13b912e8
Rab-son <rabsonsayendajnr@gmail.com>
2022-12-27 00:37:25 +0200

    code add

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ def main():
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
 
-    winnings = check_winnings(slots, lines, bet, symbol_value)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
     print(f"You won ${winnings}.")
 
 main()
