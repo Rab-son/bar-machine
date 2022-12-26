29555b0707d3d67056f4369e6c020e427b732d04
Rab-son <rabsonsayendajnr@gmail.com>
2022-12-27 00:21:59 +0200

    slots

--- main.py
+++ main.py
@@ -100,5 +100,6 @@ def main():
     print(f"You are betting ${bet} on {lines} lines. Total bet is qual to: ${total_bet}")
 
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
