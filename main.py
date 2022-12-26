d860e3ac30de30a4dd373db41a0ccd1056652869
Rab-son <rabsonsayendajnr@gmail.com>
2022-12-26 23:56:46 +0200

    total bet

--- main.py
+++ main.py
@@ -50,8 +50,9 @@ def main():
     balance = deposit()
     lines = get_number_of_lines()
     bet = get_bet()
+    total_bet = bet * lines
 
+    print(f"You are betting ${bet} on {lines} lines. Total bet is qual to: ${total_bet}")
     print(balance, lines)
-
 
 main()
